905b5be1dcd718daa16e64630756e459d4988cba
AYUSH ARYAN <144141216+ayusharyann@users.noreply.github.com>
2025-07-26 20:01:34 +0530

    Add files via upload

---
+++ WEEK4_3.WEBAPI_HANDSON/3.WEBAPI_HANDSON/Controllers/EmployeeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using WebApiLab3.Models;
+
+namespace WebApiLab3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly List<Employee> _employees;
+
+        public EmployeeController()
+        {
+            _employees = GetStandardEmployeeList();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(List<Employee>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<List<Employee>> Get()
+        {
+            // Throw exception deliberately to test exception filter
+            throw new Exception("Test exception from GET endpoint.");
+        }
+
+        private List<Employee> GetStandardEmployeeList()
+        {
+            return new List<Employee>
+            {
+                new Employee
+                {
+                    Id = 1,
+                    Name = "Anisha",
+                    Salary = 50000,
+                    Permanent = true,
+                    Department = new Department { Id = 1, Name = "HR" },
+                    Skills = new List<Skill>
+                    {
+                        new Skill { Id = 1, Name = "C#" },
+                        new Skill { Id = 2, Name = "SQL" }
+                    },
+                    DateOfBirth = new DateTime(1999, 5, 12)
+                }
+            };
+        }
+    }
+}
